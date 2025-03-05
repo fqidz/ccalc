ddb8cbda9a6c1a132271775d4c8756e2a6a3bd19
fqidz <faidz.arante@gmail.com>
2025-03-05 22:49:39 +0300

    seg fault

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -9,7 +10,10 @@ typedef enum {
     MINUS,
     MULTIPLY,
     DIVIDE,
-    ILLEGAL,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    INVALID,
+    INVALID_NUMBER,
 } TokenType;
 
 typedef struct {
@@ -23,33 +27,83 @@ void parse(Token **tokensBuf, char *string, int stringSize) {
     }
 
     int i = 0;
+    int startNumber = 0;
+    int numberLength = 0;
+    bool isCurrentlyNumber = false;
+    bool seenDot = false;
+    bool invalidNumber = false;
     while (i < strlen(string)) {
         Token *currentToken = malloc(sizeof(Token));
-        char *currentValue = malloc(2 * sizeof(char));
-        sprintf(currentValue, "%c", string[i]);
+        if (isdigit(string[i])) {
+            if (isCurrentlyNumber) {
+                numberLength++;
+            } else {
+                isCurrentlyNumber = true;
+                startNumber = i;
+            }
+
+            i++;
+            continue;
+        }
+
+        printf("number length: %i\n", numberLength);
+
+        if (string[i] == '.') {
+            if (seenDot) {
+                numberLength++;
+                invalidNumber = true;
+            } else {
+                seenDot = true;
+                numberLength++;
+
+                i++;
+                continue;
+            }
+        }
+
+
+        if (isCurrentlyNumber || invalidNumber) {
+            // We either got the entire number or it's invalid
+            currentToken->value = malloc((numberLength + 2) * sizeof(char));
+            strncpy(currentToken->value, string + startNumber, numberLength);
+            currentToken->type = NUMBER;
+
+            startNumber = 0;
+            numberLength = 0;
+            isCurrentlyNumber = false;
+            seenDot = false;
+            invalidNumber = false;
+
+            i++;
+            continue;
+        }
+
+        currentToken->value = malloc(2 * sizeof(char));
+        sprintf(currentToken->value, "%c", string[i]);
 
         switch (string[i]) {
             case '+':
-                currentToken->value = currentValue;
                 currentToken->type = PLUS;
                 break;
             case '-':
-                currentToken->value = currentValue;
                 currentToken->type = MINUS;
                 break;
             case '*':
             case 'x':
             case 'X':
-                currentToken->value = currentValue;
                 currentToken->type = MULTIPLY;
                 break;
             case '/':
-                currentToken->value = currentValue;
                 currentToken->type = DIVIDE;
                 break;
+            case '(':
+                currentToken->type = LEFT_PAREN;
+                break;
+            case ')':
+                currentToken->type = RIGHT_PAREN;
+                break;
             default:
-                currentToken->value = currentValue;
-                currentToken->type = ILLEGAL;
+                currentToken->type = INVALID;
         }
         tokensBuf[i] = currentToken;
         i++;
@@ -100,8 +154,17 @@ int main() {
             case DIVIDE:
                 printf("DIVIDE");
                 break;
-            case ILLEGAL:
-                printf("ILLEGAL");
+            case LEFT_PAREN:
+                printf("LEFT_PAREN");
+                break;
+            case RIGHT_PAREN:
+                printf("RIGHT_PAREN");
+                break;
+            case INVALID:
+                printf("INVALID");
+                break;
+            case INVALID_NUMBER:
+                printf("INVALID_NUMBER");
                 break;
             default:
                 break;
@@ -112,4 +175,3 @@ int main() {
 
     return 0;
 }
-
