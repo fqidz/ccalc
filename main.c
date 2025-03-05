29a41de064de2d3ba553a1996394e6e9c3f5c80f
fqidz <faidz.arante@gmail.com>
2025-03-05 21:01:06 +0300

    starting on parser

--- main.c
+++ main.c
@@ -1,34 +1,64 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-enum TokenType {
-    DIGIT,
+typedef enum {
+    NUMBER,
     PLUS,
     MINUS,
     MULTIPLY,
     DIVIDE,
-};
+} TokenType;
 
-struct Token {
+typedef struct {
     char *value;
-    enum TokenType type;
-};
+    TokenType type;
+} Token;
 
-struct Token* parse(char* string) {
-    enum TokenType tokens[strlen(string)];
+void parse(Token **tokensBuf, char *string, int stringSize) {
     int i = 0;
+    int tokensBufIndex = 0;
     while (i < strlen(string)) {
-        if (isdigit(string[i])) {
-            tokens[i] = DIGIT;
-        } else if ('+') {
-            tokens[i] = PLUS;
-        } else if ('-') {
-            tokens[i] = MINUS;
+        Token *currentToken = malloc(sizeof(Token));
+        if (string[i] == 0) {
+            break;
+        } else if (string[i] == '+') {
+            currentToken->value = "+";
+            currentToken->type = PLUS;
+            tokensBuf[tokensBufIndex] = currentToken;
+        } else if (string[i] == '+') {
+            currentToken->value = "+";
+            currentToken->type = PLUS;
+            tokensBuf[tokensBufIndex] = currentToken;
         }
+        // bool seenDot = false;
+        // char *current_token = malloc((stringSize + 1) * sizeof(char));
+        // if (current_token == NULL) {
+        //     // TODO: Handle error
+        // }
+        // int j = 0;
+        // while((isdigit(string[i]) || string[i] == '.') && !seenDot) {
+        //     current_token[j] = string[i];
+        //     j++;
+        // }
         i++;
     }
 }
+// 1.29+3
+// check 0;
+// : is digit
+// check 1;
+// : is dot
+// check 2;
+// : is digit
+// check 3;
+// : is digit
+// check 4;
+// : not digit
+// save 0-3 as digit
+//
 
 void tests() {
 }
