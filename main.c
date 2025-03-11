c4aae22a3a83c4305f7cf56d25f272d483175e12
fqidz <faidz.arante@gmail.com>
2025-03-11 10:45:15 +0300

    small fixes

--- main.c
+++ main.c
@@ -5,18 +5,20 @@
 
 
 int main(void) {
-    char input_string[20];
+    char input_string[256];
 
-    printf("Input: ");
+    printf("Input: \n");
     if (!fgets(input_string, sizeof(input_string) / sizeof(input_string[0]), stdin)) return 1;
     input_string[strcspn(input_string, "\n")] = 0;
 
     InputStream input_stream = {0};
     input_init(&input_stream, input_string);
 
+    printf("--------------\n");
     while (!input_is_eof(&input_stream)) {
+        size_t current_pos = input_stream.pos;
         char next_char = input_next(&input_stream);
-        printf("Pos %zu: '%c'\n", input_stream.pos, next_char);
+        printf("Pos %zu:\t'%c'\n", current_pos, next_char);
     }
 
     return 0;
